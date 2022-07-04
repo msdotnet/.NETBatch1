7069331b716adb52b30d5b9c1b3a507e6a00a7ce
Avishek Kumar <avi.net@outlook.com>
2022-07-04 19:49:43 +0530

    Create an application to read file Text to demostrate Exceptions and Exception Handling...

---
+++ Module 3 - OOP in C#/ErrorHandling/ErrorHandling/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var filePath = @"D:\Training\.NETBatch1\Module 3 - OOP in C#\ErrorHandling\ErrorHandling\TextDat.txt";
+try
+{
+   Console.WriteLine(GetFileText(filePath));
+}
+catch(Exception ex)
+{
+   Console.WriteLine("Unexpected error occured while processing your request.");
+   Console.WriteLine($"Error Details: {ex.ToString()}");
+}
+
+Console.WriteLine("Executed after file is read.");
+
+static string GetFileText(string filePath)
+{
+   try
+   {
+      return File.ReadAllText(filePath);
+   }
+   catch (DirectoryNotFoundException ex)
+   {
+      return $"The directory name supplied is mispelled or doesn't exist.\nMore details here: {ex.Message}";
+   }
+   catch (FileNotFoundException ex)
+   {
+      return $"The file name supplied is mispelled or doesn't exist.\nMore details here: {ex.Message}";
+   }
+   catch(Exception ex)
+   {
+      // Some logs here
+      throw;
+   }
+   finally
+   {
+      // Cleanup the unused resources. 
+   }
+}
