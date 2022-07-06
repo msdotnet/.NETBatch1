9f3b2acdcd1bfe6a256cf24ea77b01a1f1a75139
Avishek Kumar <avi.net@outlook.com>
2022-07-06 19:59:03 +0530

    Demostrate creating, throwing, and re-throwing exceptions

---
+++ Module 3 - OOP in C#/ErrorHandling/ErrorHandling/NotFoundException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandling
+{
+   
+   public class NotFoundException : Exception
+   {
+      public string AdditionalMessage { get; set; }
+      public NotFoundException() : base()
+      {
+      }
+      public NotFoundException(string? message) : base(message)
+      {
+      }
+      public NotFoundException(string? message, string additionalMessage) : base(message)
+      {
+         this.AdditionalMessage = additionalMessage;
+      }
+      public NotFoundException(string? message, Exception? innerException): base(message, innerException)
+      {
+
+      }
+   }
+}
--- Module 3 - OOP in C#/ErrorHandling/ErrorHandling/Program.cs
+++ Module 3 - OOP in C#/ErrorHandling/ErrorHandling/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ErrorHandling;
+
 Console.WriteLine("Hello, World!");
 
 var filePath = @"D:\Training\.NETBatch1\Module 3 - OOP in C#\ErrorHandling\ErrorHandling\TextDat.txt";
@@ -6,13 +8,34 @@ try
 {
    Console.WriteLine(GetFileText(filePath));
 }
-catch(Exception ex)
+catch (Exception ex)
 {
    Console.WriteLine("Unexpected error occured while processing your request.");
    Console.WriteLine($"Error Details: {ex.ToString()}");
 }
 
 Console.WriteLine("Executed after file is read.");
+string[] arrayData = { "A", "B", "C" };
+try
+{
+   Console.WriteLine($"Find the value at position {2 + 1}: {GetArrayValueNestedException(arrayData)}");
+}
+catch (ArgumentOutOfRangeException ex)
+{
+   Console.WriteLine(ex.Message);
+}
+
+
+var list = new List<string> { "A", "B", "C" };
+
+try
+{
+   Console.WriteLine($"Searched value in a list is: {FindValueInList(list, "D")}");
+}
+catch (NotFoundException ex)
+{
+   Console.WriteLine(ex.Message);
+}
 
 static string GetFileText(string filePath)
 {
@@ -28,7 +51,7 @@ static string GetFileText(string filePath)
    {
       return $"The file name supplied is mispelled or doesn't exist.\nMore details here: {ex.Message}";
    }
-   catch(Exception ex)
+   catch (Exception ex)
    {
       // Some logs here
       throw;
@@ -37,4 +60,42 @@ static string GetFileText(string filePath)
    {
       // Cleanup the unused resources. 
    }
+}
+
+static string GetArrayValue(string[] array, int index)
+{
+   //if (index > array.Length - 1 || index < 0)
+   //{
+   //   throw new ArgumentOutOfRangeException("Index passed is out of range", nameof(index));
+   //}
+   try
+   {
+      return array[index];
+   }
+   catch (IndexOutOfRangeException ex)
+   {
+      //logging
+      throw new ArgumentException("Index passed was invalid.", nameof(index), innerException: ex);
+   }
+}
+static string GetArrayValueNestedException(string[] array)
+{
+   try
+   {
+      return GetArrayValue(array, 3);
+   }
+   catch (ArgumentException ex)
+   {
+      Console.WriteLine(ex.Message);
+      throw new ArgumentOutOfRangeException($"Index passed was out of the range. Valid range is: 0-{array.Length - 1}", innerException: ex);
+   }
+}
+static string FindValueInList(List<string> list, string searchValue)
+{
+   var result = list.Where(l => l == searchValue).FirstOrDefault();
+   if (result == null)
+   {
+      throw new NotFoundException($"Record you are searching for doesn't exist.", $"Additional Information here, list length: {list.Count}");
+   }
+   return result;
 }
