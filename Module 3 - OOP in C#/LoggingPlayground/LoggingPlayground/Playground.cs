248f131c029cd058a69fa7588b73a3cc0990de40
Avishek Kumar <avi.net@outlook.com>
2022-07-11 18:37:52 +0530

    Create a simple application to demostrate the structured and un-structured loggging

---
+++ Module 3 - OOP in C#/LoggingPlayground/LoggingPlayground/Playground.cs
@@ -0,0 +1,39 @@
+﻿using Serilog;
+
+namespace LoggingPlayground
+{
+   public class Playground
+   {
+      public static void PlayWithLogging()
+      {
+         Log.Information("This is my first logging on console and file simultaniously.");
+
+         // Structured and un-structured logs:
+         string provider = "Google";
+         var position = new { Latitude = 25, Longitude = 134, Provider = provider };
+         var elapsedMs = 34;
+         Log.Information("Processed {@Position} in {Elapsed} ms.", position, elapsedMs);
+
+
+         var fruits = new[] { "Apple", "Orange" };
+         Log.Information("My fruite collection is: {Fruits}", fruits);
+
+
+         // Logging Debug, Warning and Exception details
+         int a = 10, b = 0;
+         try
+         {
+            Log.Debug($"Dividing {a} by {b}");
+            if (b == 0)
+            {
+               Log.Warning("You are trying to divid by zereo, it will result into failure.");
+            }
+            Console.WriteLine(a / b);
+         }
+         catch (DivideByZeroException ex)
+         {
+            Log.Error(ex, $"Something went wrong. Error: {ex.Message}");
+         }
+      }
+   }
+}
---
+++ Module 3 - OOP in C#/LoggingPlayground/LoggingPlayground/Program.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+using Serilog.Formatting.Json;
+using LoggingPlayground;
+
+// Instantiate and initialize the logger
+Log.Logger = new LoggerConfiguration()
+   .MinimumLevel.Debug()
+   .WriteTo.Console()
+   .WriteTo.File("logs/myloggingapp-human-readable.txt", rollingInterval: RollingInterval.Day)
+   .WriteTo.File(new JsonFormatter(), "logs/myloggingapp-machine-readable.txt", rollingInterval: RollingInterval.Day)
+   .CreateLogger();
+
+Log.Information("Logger is intantiated and initialized.");
+
+
+try
+{
+   Playground.PlayWithLogging();
+}
+finally
+{
+   Log.CloseAndFlush();
+}
+
