4f97cd8eec49da9e560aa64b5370aa0631dad83b
Avishek Kumar <avi.net@outlook.com>
2022-08-02 19:41:55 +0530

    Create a sample async playground app and demostrate how to use async and await for CPU and IO bound operations.

---
+++ Module 4 - Collections, LINQ and Async Programming/AsyncPlayground/AsyncPlayground.Library/NetworkService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AsyncPlayground.Library
+{
+   public class NetworkService
+   {
+      private readonly HttpClient _client;
+      public NetworkService()
+      {
+         _client = new HttpClient();
+         _client.BaseAddress = new Uri("https://devblogs.microsoft.com/dotnet/");
+      }
+      public string ReadWebAsString()
+      {
+         Task.Delay(3000).Wait();
+         return _client.GetStringAsync(_client.BaseAddress).GetAwaiter().GetResult();
+      }
+      public async Task<string> ReadWebAsStringAsync()
+      {
+         await Task.Delay(5000);
+         return await _client.GetStringAsync(_client.BaseAddress);
+      }
+      public async Task<int> FindNetFromWebResultAsync(string htmlString)
+      {
+         return await Task.Run(() => Regex.Match(htmlString, "NET").Length);
+      }
+
+   }
+}
---
+++ Module 4 - Collections, LINQ and Async Programming/AsyncPlayground/AsyncPlayground/Program.cs
@@ -0,0 +1,47 @@
+﻿using AsyncPlayground.Library;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AsyncPlayground
+{
+   internal class Program
+   {
+      static async Task Main(string[] args)
+      {
+         var networkService = new NetworkService();
+         var webStringTask = networkService.ReadWebAsStringAsync(); //  Async - returns Task<string>
+         var stringHtmlSync = networkService.ReadWebAsString();
+         var findNetTask = networkService.FindNetFromWebResultAsync(stringHtmlSync); // Async - returns Task<int>
+         Console.WriteLine("Web String async task started on worker thread");
+         Console.WriteLine(stringHtmlSync);
+
+         //await Task.WhenAll(webStringTask, findNetTask);
+         var taskCollection = new List<Task>();
+         taskCollection.Add(findNetTask);
+         taskCollection.Add(findNetTask);
+
+         await Task.WhenAll(taskCollection);
+
+         //while (taskCollection.Count > 0)
+         //{
+         //   var task = await Task.WhenAny(taskCollection);
+         //   if (task == webStringTask)
+         //   {
+         //      Console.WriteLine("Web String Task has been finished")
+         //   }
+         //   else if (task == findNetTask)
+         //   {
+         //      Console.WriteLine("Find Net Task has been finished");
+         //   }
+         //}
+
+
+
+         Console.WriteLine(webStringTask.Result);
+
+         Console.WriteLine(findNetTask.Result);
+         Console.WriteLine("Operation after async call");
+      }
+   }
+}
